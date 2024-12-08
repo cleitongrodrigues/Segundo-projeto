51deaf4e9eba652b03b80265561e976ce99408c8
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-12-08 10:42:09 -0300

    Treinando Scanner

--- Variaveis/src/App.java
+++ Variaveis/src/App.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int peso;
-        System.out.println("Digite seu peso: ");
+        Double peso;
+        Double altura;
+        Double imc;
+
+        System.out.println("Digite seu peso");
         Scanner p = new Scanner(System.in);
-        peso = p.nextInt();
+        peso = p.nextDouble();
 
-        String nome;
-        System.out.println("Qual o seu nome?");
-        Scanner name = new Scanner(System.in);
-        nome = name.next();
+        System.out.println("Digite sua altura");
+        Scanner a = new Scanner(System.in);
+        altura = a.nextDouble();
 
-        System.out.println("Oi " + nome + " seu peso é " + peso);
+        imc = peso / (altura * altura);
+
+        System.out.println("Seu peso é: " + peso + " e sua altura é " + altura + " e seu IMC é: " + imc);
     }
 }
